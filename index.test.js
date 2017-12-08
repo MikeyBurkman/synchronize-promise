6939b0c50472788b53e4cbdad8a629546cfe2585
MikeyBurkman <michael.burkman@gmail.com>
2017-12-08 15:02:08 -0500

    Fix up tests

--- index.test.js
+++ index.test.js
@@ -5,35 +5,39 @@ const singleThread = require('./index');
 
 it('Should only allow one call at a time to a given function', () => {
 
-    var alreadyRunning = false;
-    var processingOrder = [];
+    const startTimes = {};
+    const endTimes = {};
 
     // f is our function that we expect to only be executed one at a time.
-    var f = singleThread((x) => {
-      if (alreadyRunning) {
-        return Bluebird.reject(new Error('The service was already running while processing value ' + x));
-      }
-      alreadyRunning = true;
-      processingOrder.push(x);
+    const f = singleThread((x) => {
+      startTimes[x] = Date.now();
       return Bluebird.delay(25)
         .then(function () {
-          alreadyRunning = false;
+          endTimes[x] = Date.now();
           return x;
         });
     });
 
+    const startTime = Date.now();
     return Bluebird.all([
-      f(1),
-      f(2),
-      f(3)
+      f('a'),
+      f('b'),
+      f('c')
     ]).then(function (results) {
-      expect(results).toEqual([1, 2, 3]); // Make sure each function returns the right value
-      expect(processingOrder).toEqual([1, 2, 3]); // Make sure they're executed in the right order
+      expect(results).toEqual(['a', 'b', 'c']); // Make sure each function returns the right value
+
+      expect(endTimes['a']).toBeGreaterThan(startTimes['a']); // Sanity check
+      expect(startTimes['b']).toBeGreaterThanOrEqual(endTimes['a']); // B needs to start after A has ended
+      expect(startTimes['c']).toBeGreaterThanOrEqual(endTimes['b']); // C needs to start after B has ended
+
+      // If we actually performed this in sequence, then the total time should be at
+      //  least (time for one function) * (time per function) = 25*3
+      expect(Date.now() - startTime).toBeGreaterThan(75);
     });
   });
 
   it('Should handle one of the calls failing', () => {
-    var f = singleThread((x) => {
+    const f = singleThread((x) => {
       return Bluebird.delay(25).then(() => {
         if (x > 1) {
           throw new Error('x was greater than 1');
@@ -43,11 +47,11 @@ it('Should only allow one call at a time to a given function', () => {
       });
     });
 
-    var p1 = f(1); // This should succeed
-    var p2 = f(2) // This we expect to fail
+    const p1 = f(1); // This should succeed
+    const p2 = f(2) // This we expect to fail
       .reflect()
       .then((res) => {
-        var err = res.reason();
+        const err = res.reason();
         expect(err.message).toEqual('x was greater than 1');
       });
 
